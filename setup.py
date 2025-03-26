1a9d2807481354bfe4da1990ca1386c333cd94cd
Shyam <bansalshyam17@gmail.com>
2025-03-26 19:19:29 +0530

    added cookiecutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='This project is to understand the concepts regarding the MLops.',
+    author='shyam',
+    license='',
+)
